a15140f75d3ae756f88535bfb950fdd5b37c1a51
James Zhang <jameszhang2019@outlook.com>
2024-05-27 18:46:12 -0400

    Test GET and POST requests with axios and Spring

--- backend/src/main/java/application/Controller.java
+++ backend/src/main/java/application/Controller.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 public class Controller {
     @CrossOrigin(origins = "http://localhost:5173")
@@ -11,5 +13,19 @@ public class Controller {
     public ResponseEntity<String> index() {
         System.out.println("Hello world!");
         return new ResponseEntity<>("Hello world!", HttpStatus.OK);
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @GetMapping("/button")
+    public ResponseEntity<String> button() {
+        System.out.println("Button clicked!");
+        return new ResponseEntity<>("Received!", HttpStatus.OK);
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @PostMapping("/text")
+    public ResponseEntity<String> getText(@RequestBody Map<String, String> body) {
+        System.out.println(body.get("text"));
+        return new ResponseEntity<>("Received!", HttpStatus.OK);
     }
 }
--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,8 +1,56 @@
+import { useState } from 'react';
 import './App.css'
+import axios from 'axios';
 import Board from './Board.jsx'
 
 function App() {
-  return <Board />;
+  const [msg, setMsg] = useState("initial");
+  hello(setMsg);
+  return <>
+    <h1>{msg}</h1>
+    <button onClick={buttonClick}>Click Me!</button><br />
+    <input type="text" id="textbox" /><br />
+    <button onClick={send}>Send</button>
+  </>
+  // return <Board />;
+}
+
+function hello(setMsg) {
+  axios.get('http://localhost:8080/',)
+    .then((response) => {
+      setMsg(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function buttonClick() {
+  axios.get('http://localhost:8080/button',)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function send() {
+  const textbox = document.getElementById("textbox");
+  console.log(textbox.value);
+  axios.post('http://localhost:8080/text', {
+    text: textbox.value
+  }, {
+    headers: {}  // Add headers here if needed
+  })
+    .then((response) => {
+      console.log(response);
+      console.log(response.data);
+      // Handle data
+    })
+    .catch((error) => {
+      console.log(error);
+    })
 }
 
 export default App
